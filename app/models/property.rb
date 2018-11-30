e1f9bb71f7a750d089db46254151761719eecad1
Serhiy <sergiy.onyshchuk@gmail.com>
2018-11-30 14:07:02 +0100

    Added Geolocation to detect address

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,7 +5,7 @@ class Property < ApplicationRecord
   def generate_final_template
     analyzed_photos   = process_rooms_photos
     geo_coordinates   = detect_geo_coordinates(rooms.to_a.map(&:photos).flatten)
-    property_id       = generate_property_id(geo_coordinates)
+    address           = detect_address(geo_coordinates)
     price_per_night   = generate_price_per_night(geo_coordinates)
     general_amenities = detect_general_amenities(analyzed_photos)
     description       = generate_description(analyzed_photos)
@@ -16,17 +16,26 @@ class Property < ApplicationRecord
   end
 
   def generate_price_per_night
-    #photos.map(&:detect_geo_coordinates).compact.first(3)
     main_url = "http://87.233.77.180:81/api/v1/properties?persons=#{persons}&nights=#{7}&exact_location=1&facets=_default%2Cbathrooms%2Cstars%2Cwith_discount&fields=id%2Cprice&locale=en&page=1&per_page=300&platform=bvmobapp"
     polygon_points = []
-    ["4.4752#{rand(9)},52.2702#{rand(9)}","4.4752#{rand(9)},52.2702#{rand(9)}","4.4752#{rand(9)},52.2702#{rand(9)}"].each_with_index{|c,i|
-      "polygon_points[#{i}]=#{c}"
+    photos.map(&:detect_geo_coordinates).compact.first(4).compact.each_with_index{|c,i|
+      polygon_points << "polygon_points[#{i}]=#{c.join(',')}"
     }
     result        = JSON.parse(RestClient.get(main_url + '&' + polygon_points.join('&')))
     prices        = result['properties'].map{|p| p['price']['price']}
     average_price = prices.sum / prices.size.to_f
     average_price = (average_price / 7).round
     average_price - (average_price * 0.20)
+  end
+
+  def detect_address
+    if data = Geocoder.search(geo_coordinates.reverse).first.try(:data)
+      {
+        country: data['address']['country'],
+        region: data['address']['state'],
+        city: data['address']['city'],
+      }
+    end
   end
 
 end
--- db/migrate/20181130095042_create_properties.rb
+++ db/migrate/20181130095042_create_properties.rb
@@ -1,12 +1,14 @@
 class CreateProperties < ActiveRecord::Migration[5.2]
   def change
     create_table :properties do |t|
-      t.string :property_id
       t.string :geo_coordinates
       t.text :description
       t.integer :persons
       t.text :general_amenities
       t.float :price_per_night
+      t.string :country
+      t.string :region
+      t.string :city
 
       t.timestamps
     end
