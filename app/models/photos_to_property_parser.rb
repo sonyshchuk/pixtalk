90084286978d9e41a159a181998967be09252b0b
Serhiy <sergiy.onyshchuk@gmail.com>
2018-11-30 12:12:11 +0100

    added photos analyzer lib

--- app/models/photos_to_property_parser.rb
+++ app/models/photos_to_property_parser.rb
@@ -1,11 +1,9 @@
-namespace :recognize_image do
+class PhotosAnalyzer
 
-  desc "Provide translation"
-  task now: :environment do
-    file_path = [Rails.root,'public','table.jpg'].join('/')
+  def self.recognize(photo_path)
     cmd = TTY::Command.new
     tensor_path = [Rails.root,'lib','tensor_models','tutorials','image','imagenet'].join('/')
-    out,err = cmd.run("cd #{tensor_path} && python3 classify_image.py --image_file=#{file_path}")
+    out,err = cmd.run("cd #{tensor_path} && python3 classify_image.py --image_file=#{photo_path}")
     result = []
     out.split(', ').map{|i|
       i.split("\n").map{|y| 
