c93c3f9af31547156a0241b4a8feb798656eb7e7
Serhiy <sergiy.onyshchuk@gmail.com>
2018-11-30 16:12:34 +0100

    room json response change

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,8 +3,16 @@ class Room < ApplicationRecord
   has_many :photos
   attr_accessor :photo_data
 
+  def as_json(attrs={})
+    {
+      name: name,
+      amenities: amenities.split(','),
+      photos: photos.to_a.map{|p| p.path }
+    }
+  end
+
   def process_photos!
-    self.amenities = photos.to_a.map{|p| PhotosAnalyzer.recognize(p.path.path) }.flatten
+    self.amenities = photos.to_a.map{|p| PhotosAnalyzer.recognize(p.path.path) }.flatten.join(',')
     self.save
     self.amenities
   end
